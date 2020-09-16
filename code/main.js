7b68430f5e0099a2ce70f651c724c4200e7727b8
Zachary Olpin <info@zacharyolp.in>
2020-09-15 21:21:44 -0600

    clone a repo and run cloc on it. Problems: does not remove directory or validate req body before calling exec

--- code/main.js
+++ code/main.js
@@ -1,10 +1,42 @@
-import express from 'express';
+const express = require('express');
+const server = express();
+const { execSync } = require('child_process')
 
-const PORT = 3000;
+const PORT = '5000';
 
-const app = express();
+const cloneRepo = (gitUrl) => {
+  const success = execSync(`git clone ${gitUrl}`, (err, stdout, stderror) => {
+    if (err || stderror) return false;
+    return true;
+  });
+  return success;
+}
 
-// Middleware for static directory.
-app.use(express.static("static"));
+const runCloc = (repoPath) => {
+  const output = execSync(`cloc ${process.cwd()}/${repoPath}`, (err, stdout, stderror) => {
+    if (err) return err.message;
+    if (stderror) return stderror;
+    return stdout;
+  })
+  return output;
+};
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+
+server.get('/', (req, res) => {
+  res.send('hello');
+});
+
+server.post('/analyze', (req, res) => {
+  const { gitUrl } = req.body;
+  const repoName = gitUrl.match(/\/([\w\-\_\d]*).git$/)[1];
+  if (cloneRepo(gitUrl)){
+    res.send(runCloc(repoName));
+  } else {
+    res.status(500).json({message: "Something went wrong."});
+  }
+  // TODO: cleanup. Remove cloned dir.
+});
+
+server.listen(PORT, () => console.log(`Server is running at ${PORT}...`));
