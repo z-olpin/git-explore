4b14f3d6ac891c3dc97e2214e893b842d2bf5b03
Zachary Olpin <info@zacharyolp.in>
2020-09-15 21:53:11 -0600

    frontend

--- code/main.js
+++ code/main.js
@@ -1,42 +1,46 @@
-const express = require('express');
-const server = express();
-const { execSync } = require('child_process')
+const express = require("express")
+const server = express()
+const { execSync } = require("child_process")
+const path = require("path")
+const fs = require("fs")
 
-const PORT = '5000';
+const PORT = "5000"
 
 const cloneRepo = (gitUrl) => {
   const success = execSync(`git clone ${gitUrl}`, (err, stdout, stderror) => {
-    if (err || stderror) return false;
-    return true;
-  });
-  return success;
+    if (err || stderror) return false
+    return true
+  })
+  return success
 }
 
-const runScc = (repoPath) => {
-  const output = execSync(`scc ${process.cwd()}/${repoPath}`, (err, stdout, stderror) => {
-    if (err) return err.message;
-    if (stderror) return stderror;
-    return stdout;
-  })
-  return output;
-};
+const runTokei = (repoPath) => {
+  const output = execSync(
+    `tokei -o json ${process.cwd()}/${repoPath}`,
+    (err, stdout, stderror) => {
+      if (err) return err.message
+      if (stderror) return stderror
+      return stdout
+    }
+  )
+  return output.toString();
+}
 
-server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.static(path.join(__dirname, "client")))
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
 
-server.get('/', (req, res) => {
-  res.send('hello');
-});
-
-server.post('/analyze', (req, res) => {
-  const { gitUrl } = req.body;
-  const repoName = gitUrl.match(/\/([\w\-\_\d]*).git$/)[1];
-  if (cloneRepo(gitUrl)){
-    res.send(runScc(repoName));
+server.post("/analyze", (req, res) => {
+  const { gitUrl } = req.body
+  const repoName = gitUrl.match(/\/([\w\-\_\d]*).git$/)[1]
+  if (cloneRepo(gitUrl)) {
+    res.send(runTokei(repoName))
   } else {
-    res.status(500).json({message: "Something went wrong."});
+    res.status(500).json({ message: "Something went wrong." })
   }
-  // TODO: cleanup. Remove cloned dir.
-});
+  fs.rmdir(repoName, { recursive: true }, (err) => {
+    if (err) throw err
+  })
+})
 
-server.listen(PORT, () => console.log(`Server is running at ${PORT}...`));
+server.listen(PORT, () => console.log(`Server is running at ${PORT}...`))
