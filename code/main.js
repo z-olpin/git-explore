8203175a769059863a8dc075b2c55e3a559483ba
Abhi Sohal <abbbbbbbbbbbhi@gmail.com>
2020-09-18 03:34:44 -0700

    set up basic express app, create html + client js file .

--- code/main.js
+++ code/main.js
@@ -1,7 +1,28 @@
-console.log("starting app");
-let counter = 0
-setInterval(() => {
-    console.log(counter+=1);
-}, 1000);
+import { readFileSync } from 'fs';
+import { createServer } from 'http';
+
+import WebSocket from 'ws';
+import express from 'express';
+
+
+const PORT = 3000;
+
+// for ssl
+// const server = https.createServer({
+//     cert: fs.readFileSync('/path/to/cert.pem'),
+//     key: fs.readFileSync('/path/to/key.pem')
+//   });
+
+const app = express();
+app.use('/static', express.static('static')) //path should look like http://localhost:3000/static/css/style.css
+app.get('/', (req, res) => {
+    res.redirect('/static/index.html') //send to home page as default 
+});
+
+const server = app.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+});
+
+
 
 
