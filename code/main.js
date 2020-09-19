2e12d75af3d14067b0ff691943f856bde2cad7fd
Zachary Olpin <info@zacharyolp.in>
2020-09-19 04:22:28 -0600

    avoid extra redirect and leverage middleware for static resources

--- code/main.js
+++ code/main.js
@@ -4,9 +4,7 @@ const PORT = 3000;
 
 const app = express();
 
-app.use('/static', express.static('static')) //path should look like http://localhost:3000/static/css/style.css
-app.get('/', (req, res) => {
-    res.redirect('/static/index.html') //send to home page as default 
-});
+// Middleware for static directory.
+app.use(express.static("static"));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
