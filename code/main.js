85821e1186dc000177dfdbc8bffb0e0f83aa370b
Zachary Olpin <info@zacharyolp.in>
2020-09-19 03:36:14 -0600

    cleanup

--- code/main.js
+++ code/main.js
@@ -1,22 +1,12 @@
-import { readFileSync } from 'fs';
-import { createServer } from 'http';
-
-import WebSocket from 'ws';
 import express from 'express';
-
 
 const PORT = 3000;
 
 const app = express();
+
 app.use('/static', express.static('static')) //path should look like http://localhost:3000/static/css/style.css
 app.get('/', (req, res) => {
     res.redirect('/static/index.html') //send to home page as default 
 });
 
-const server = app.listen(PORT, () => {
-    console.log('Server started on port ' + PORT);
-});
-
-
-
-
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
