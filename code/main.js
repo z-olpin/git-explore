b2af4c00e1c4440ae3735dd6c8c558600b922f91
Zachary Olpin <info@zacharyolp.in>
2020-09-22 00:34:08 -0600

    clone and tokei functions with promisified execFile

--- code/main.js
+++ code/main.js
@@ -1,51 +1,48 @@
-const express = require("express")
-const server = express()
-const { execSync } = require("child_process")
+const util = require('util')
+const execFile = util.promisify(require("child_process").execFile)
 const path = require("path")
 const fs = require("fs")
+const express = require("express")
 
-// TODO error handling and attention to security
+const server = express()
 
-// Temporarily hard coded
-const PORT = "5000"
-
-// Super disk / cpu / network intensive.
-const cloneRepo = (gitUrl) => {
-  const success = execSync(`git clone --depth 1 ${gitUrl}`, (err, stdout, stderror) => {
-    if (err || stderror) return false
-    return true
-  })
-  return success
-}
-
-const runTokei = (repoPath) => {
-  const output = execSync(
-    `tokei -o json ${process.cwd()}/${repoPath}`,
-    (err, stdout, stderror) => {
-      if (err) return err.message
-      if (stderror) return stderror
-      return stdout
-    }
-  )
-  return output.toString();
-}
+const PORT = 3000
 
 server.use(express.static(path.join(__dirname, "client")))
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
-server.post("/analyze", (req, res) => {
+// Clones a repository to the process's cwd
+const clone = async (gitUrl) => {
+  const { stdout } = await execFile('git', ['clone', '--depth=1', gitUrl])
+    .catch(e => {
+      throw e
+    });
+  return
+}
+
+// Runs the tokei utility on @param `path` and returns default analysis as an object.
+const tokei = async (repoPath) => {
+  const { stdout } = await execFile('tokei', ['-o', 'json', repoPath])
+    .catch(e => {
+      throw e
+    });
+    return JSON.parse(stdout)
+}
+
+/*****************************************
+ *               ROUTES                  *
+ *****************************************/
+ 
+// Post request with `gitUrl` field in body returns tokei analysis as json object
+server.post("/analyze", async (req, res) => {
   const gitUrl = req.body.gitUrl.replace(/[^a-zA-Z0-9/_\.\-:]/g, '')
   const repoName = gitUrl.match(/\/([\w\-\_\d]*).git$/)[1]
-  if (repoName && cloneRepo(gitUrl)) {
-    res.send(runTokei(repoName))
-  } else {
-    res.status(500).json({ message: "Something went wrong." })
-  }
-  // TODO
-  fs.rmdir(repoName, { recursive: true }, (err) => {
-    if (err) throw err
-  })
+  await clone(gitUrl)
+    .catch(e => console.error(e))
+  const analysis = await tokei(repoName)
+    .catch(e => console.error(e))
+  res.json(analysis)
 })
 
 server.listen(PORT, () => console.log(`Server is running at ${PORT}...`))
